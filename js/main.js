f04d9a3afb9465d80e6e5ed8a392bfeccdc15a8f
Ariel Burriss <ariburr@gmail.com>
2017-11-16 22:21:00 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,52 @@
+var keys = document.querySelectorAll(".key");
+var output = document.querySelector("#output");
+var clear =  document.querySelector("#clear");
+var firstVal;
+var secondVal;
+var operator;
+var total = 0;
+
+keys.forEach(function(key){
+  key.addEventListener("click", function(){
+    if ((/\*|X|\/|-|\+|\./g).test(key.id)) {
+      firstVal = output.textContent;
+      operator = key.id;
+      output.textContent = ""
+    } else if (key.id == "=") {
+      secondVal = output.textContent;
+      output.textContent = ""
+      total = calculation();
+      output.append(total);
+    } else {
+      output.append(key.id);
+    }
+  });
+});
+
+clear.addEventListener("click", function(){
+  reset();
+});
+
+function calculation() {
+  firstVal = parseInt(firstVal);
+  secondVal = parseInt(secondVal);
+  switch (operator) {
+    case "+":
+      return firstVal + secondVal;
+      break;
+    case "-":
+      return firstVal - secondVal;
+      break;
+    case "*":
+      return firstVal * secondVal;
+      break;
+    case "/":
+      return firstVal / secondVal;
+  }
+}
+
+function reset() {
+  output.textContent = ""
+  firstVal = 0;
+  secondVal = 0;
+}
